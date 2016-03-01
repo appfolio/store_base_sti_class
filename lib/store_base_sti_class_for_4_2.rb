d94d893d3e296b1671250a3e394ca39fd9adda47
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-03-01 21:04:23 +0100

    This patch fixes STI class when methods are called on main Class.
    Example:
    
    class Account < ApplicationRecord
    end
    
    class Customer < Account
    end
    
    class Prospect < Account
    end
    
    Now Customer.joins(:comments).count returns :
    
    SELECT COUNT(*) FROM "accounts"
    INNER JOIN "comments" ON "comments"."commentable_id" = "accounts"."id"
    AND "comments"."commentable_type" = $1
    WHERE "accounts"."type" IN ('Customer')  [["commentable_type", "Account"]]
    
    With this patch is returns :
    
    SELECT COUNT(*) FROM "accounts"
    INNER JOIN "comments" ON "comments"."commentable_id" = "accounts"."id"
    AND "comments"."commentable_type" = $1
    WHERE "accounts"."type" IN ('Customer')  [["commentable_type", "Customer"]]

--- lib/store_base_sti_class_for_4_2.rb
+++ lib/store_base_sti_class_for_4_2.rb
@@ -152,7 +152,7 @@ if ActiveRecord::VERSION::STRING =~ /^4\.2/
 
           if reflection.type
             # BEGIN PATCH
-            # original: 
+            # original:
             # value    = next_reflection.klass.base_class.name
             # bind_val = bind scope, table.table_name, reflection.type, value, tracker
             # scope    = scope.where(table[reflection.type].eq(bind_val))
@@ -166,7 +166,7 @@ if ActiveRecord::VERSION::STRING =~ /^4\.2/
               scope    = scope.where(table[reflection.type].in(([klass] + klass.descendants).map(&:name)))
             end
             # END PATCH
-            
+
           end
 
           scope = scope.joins(join(foreign_table, constraint))
@@ -191,7 +191,73 @@ if ActiveRecord::VERSION::STRING =~ /^4\.2/
             scope
           end
         end
-        
+
+      end
+
+      class JoinDependency
+        class JoinAssociation
+          def join_constraints(foreign_table, foreign_klass, node, join_type, tables, scope_chain, chain)
+            joins         = []
+            bind_values   = []
+            tables        = tables.reverse
+
+            scope_chain_index = 0
+            scope_chain = scope_chain.reverse
+
+            # The chain starts with the target table, but we want to end with it here (makes
+            # more sense in this context), so we reverse
+            chain.reverse_each do |reflection|
+              table = tables.shift
+              klass = reflection.klass
+
+              join_keys   = reflection.join_keys(klass)
+              key         = join_keys.key
+              foreign_key = join_keys.foreign_key
+
+              constraint = build_constraint(klass, table, key, foreign_table, foreign_key)
+
+              scope_chain_items = scope_chain[scope_chain_index].map do |item|
+                if item.is_a?(Relation)
+                  item
+                else
+                  ActiveRecord::Relation.create(klass, table).instance_exec(node, &item)
+                end
+              end
+              scope_chain_index += 1
+
+              scope_chain_items.concat [klass.send(:build_default_scope, ActiveRecord::Relation.create(klass, table))].compact
+
+              rel = scope_chain_items.inject(scope_chain_items.shift) do |left, right|
+                left.merge right
+              end
+
+              if rel && !rel.arel.constraints.empty?
+                bind_values.concat rel.bind_values
+                constraint = constraint.and rel.arel.constraints
+              end
+
+              if reflection.type
+                # BEGIN PATCH
+                # original: foreign_klass.base_class.name
+                value  = ActiveRecord::Base.store_base_sti_class ? foreign_klass.base_class.name : foreign_klass.name
+                # END PATCH
+
+                column = klass.columns_hash[reflection.type.to_s]
+
+                substitute = klass.connection.substitute_at(column)
+                bind_values.push [column, value]
+                constraint = constraint.and table[reflection.type].eq substitute
+              end
+
+              joins << table.create_join(table, table.create_on(constraint), join_type)
+
+              # The current table in this iteration becomes the foreign table in the next
+              foreign_table, foreign_klass = table, klass
+            end
+
+            JoinInformation.new joins, bind_values
+          end
+        end
       end
 
       module ThroughAssociation
