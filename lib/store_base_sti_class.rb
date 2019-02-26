a4f541eeb662f2b797a2e8572fd0731c4a4a30bd
Nicolas Rodriguez <nicoladmin@free.fr>
2019-02-26 12:05:29 +0100

    Drop support of Rails 4.0.x, 4.1.x and Ruby 2.2

--- lib/store_base_sti_class.rb
+++ lib/store_base_sti_class.rb
@@ -1,10 +1,6 @@
 require 'active_record'
 
-if ActiveRecord::VERSION::STRING =~ /^4\.0/
-  require 'store_base_sti_class_for_4_0'
-elsif ActiveRecord::VERSION::STRING =~ /^4\.1/
-  require 'store_base_sti_class_for_4_1'
-elsif ActiveRecord::VERSION::STRING =~ /^4\.2/
+if ActiveRecord::VERSION::STRING =~ /^4\.2/
   require 'store_base_sti_class_for_4_2'
 elsif ActiveRecord::VERSION::STRING =~ /^5\.0/
   require 'store_base_sti_class_for_5_0'
