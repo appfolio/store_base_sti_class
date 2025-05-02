ae3ce6e1166b0300d1ad40d682d2bb48f4d26fb4
Nicholas Lee <nicholas.lee@appfolio.com>
2025-05-02 12:27:23 -0700

    Appraise ruby 3.4 and rails 8

--- test/helper.rb
+++ test/helper.rb
@@ -18,6 +18,7 @@ if ENV['WITH_COVERAGE'] == 'true'
   end
 end
 
+require 'logger' # Due to issue with activesupport / concurrent-ruby
 require 'store_base_sti_class'
 require 'debug'
 require 'minitest/autorun'
